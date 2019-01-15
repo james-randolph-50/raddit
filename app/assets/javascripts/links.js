4e25c23971279c15fbeae4fb22805b7b9483d540
James <james.randolph2016@gmail.com>
2019-01-15 12:07:21 -0500

    show comments working. style added

--- app/assets/javascripts/links.js
+++ app/assets/javascripts/links.js
@@ -109,9 +109,9 @@ class Link {
 Link.prototype.formatHtml = function () {
 	return (`
 		<div>
-			<a href=${this.url}>${this.title}</a><br>
-			<div class="comments_section"></div>
-			<button class='load_comments' id=${this.id}>Show comments</button>
+			<a href=${this.url} style="font-size: 2em;">${this.title}</a><br>
+			<div class="comments_section"></div><br>
+			<button class='load_comments' id=${this.id}>Show comments</button><br><hr>
 		</div>
 	`)
 }

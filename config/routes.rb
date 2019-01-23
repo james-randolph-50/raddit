1486403b11db7e8e6ada666cbc8c8c759e6648fb
James <james.randolph2016@gmail.com>
2019-01-23 17:42:34 -0500

    trying different approach

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,8 @@ Rails.application.routes.draw do
     resources :comments
   end
   
-
+  post 'links/{:id}/comments' => "comments#create"
+  
   root to: "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
